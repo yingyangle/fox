0c7d20557520ec30909fc3069ad629a2f8c171ac
yingyangle <cyang1999@gmail.com>
2021-05-10 04:09:36 -0400

    add text to fire and devil sects; add icons

--- citations.js
+++ citations.js
@@ -28,24 +28,29 @@ function getCitations() {
 		var source = $(this).attr('data-source')
 		if (!(source in sources_dict)) source = 999
 		else source = sources_dict[source]
-		$(this).html(source+1)
-		// hover text
-		$(this).on('mouseover mousemove', () => {
-			var tooltip_text = $(this).attr('data-tooltip')
-			console.log(tooltip_text)
-			$('#tooltip').html(tooltip_text)
-		})
+		// set reference number as citation text
+		if (!$(this).hasClass('text-citation')) {
+			$(this).html(source+1)
+			// hover text
+			$(this).on('mouseover mousemove', () => {
+				var tooltip_text = $(this).attr('data-tooltip')
+				console.log(tooltip_text)
+				$('#tooltip').html(tooltip_text)
+			})
+		}
 
 		// find source in bibliography
 		if ($(this).hasClass('sources-citation')) {
 			$(this).on('click', () => {
-				// save this spot
-				save_spot = this
 				// highlight selected source
 				source = parseInt(source)
 				var ref = sources_elements[source]
+				// console.log(ref)
 				$(ref).addClass('source-highlighted')
-				console.log(ref)
+				// console.log(ref)
+
+				// save this spot
+				save_spot = this
 
 				// scroll to selected source
 				$('html, body').animate({
@@ -68,17 +73,17 @@ function getCitations() {
 	})
 
 	// citation tooltip hover
-	$('.citation')
-	.on('mouseover mousemove', () => {
-		$('#tooltip')
-			.addClass('tooltip-hover')
-			.css('left', (event.pageX - $('#tooltip').width() / 2 - 10) + 'px')
-			.css('top', (event.pageY - $('#tooltip').height() - 40) + 'px')
-	})
-	.on('mouseout', () => {
-		$('#tooltip')
-			.removeClass('tooltip-hover')
-	})
+	$('.citation:not(.text-citation)')
+		.on('mouseover mousemove', () => {
+			$('#tooltip')
+				.addClass('tooltip-hover')
+				.css('left', (event.pageX - $('#tooltip').width() / 2 - 10) + 'px')
+				.css('top', (event.pageY - $('#tooltip').height() - 40) + 'px')
+		})
+		.on('mouseout', () => {
+			$('#tooltip')
+				.removeClass('tooltip-hover')
+		})
 
 	// un-highlight selected source if click anywhere outside the highlighted source
 	$(document).click(function (event) {
